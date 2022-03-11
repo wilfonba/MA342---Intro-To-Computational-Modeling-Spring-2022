be3b9cde6368a644bd39b077e5cfda780313aa8f
Bruner <alanjbruner@gmail.com>
2022-03-11 14:36:25 -0500

    wake trail added to constant distant points

--- Project01/nPoints.m
+++ Project01/nPoints.m
@@ -1,4 +1,4 @@
-function [points] = nPoints(fileName,n)
+function [points] = nPoints(fileName,n,wake_angle,wake_len)
 
 img = rgb2gray(imread(fileName));
 edges = uint8(edge(img,"Sobel"))*255;
@@ -10,30 +10,53 @@ perim = bwperim(filled);
 pixelCount = length(find(perim~=0));
 [row,col] = find(perim~=0);
 traced = bwtraceboundary(perim,[row(end) col(end)],'N');
-blank = zeros(size(img));
+
 % placeholder point finder
 points = traced(1:round(pixelCount/n):end,:);
-for i=1:n
+points(end+1,:) = points(1,:);
+points(end+1,:) = [ ...
+        round(points(end,1)+sind(wake_angle)*wake_len) ...
+        round(points(end,2)+cosd(wake_angle)*wake_len) ...
+];
+blank = zeros(size(img)*2);
+for i=1:length(points)
     blank(points(i,1),points(i,2)) = 1;
 end
 % try using angle tolerances
 % find starting angle off of a point
 % Create new point if outside of a specific angle tolerance
 % Change angle tolerance until desired number of points is reached
-% angle_tol = 90;
+% angle_tol = 180;
+% angle_range = 180;
 % while length(points)~= n
 %     points = [row(end) col(end)];
-%     current_angle = 0;
+%     test_angle = atan2d(points(end,1)-traced(5,1),points(end,2)-traced(5,1));
 %     for i=1:length(traced)
-%         current_angle = atan2d(points(end,1)-traced(i,1),points(end,2)-traced(end,1));
-%         
+%         current_angle = atan2d(points(end,1)-traced(i,1),points(end,2)-traced(i,2));
+%         if abs(test_angle-current_angle) > angle_tol
+%            points(end+1,:) = [traced(i,1) traced(i,2)];
+%            added=i+10;
+%            if added>length(traced)
+%               added = length(traced); 
+%            end
+%            test_angle = atan2d(points(end,1)-traced(added,1),points(end,2)-traced(added,1));
+%         end
 %     end
-%     len = length(points);
+%     [len,~] = size(points);
+%     disp(points);
+%     fprintf("points: %d angle_tol: %d angle_range: %d\n",len,angle_tol,angle_range);
+%     blank = zeros(size(img));
+%     for k=1:len
+%         blank(points(k,1),points(k,2)) = 1;
+%     end
+%     imshow(imdilate(blank,se));
+%     pause
 %     if len>n
-%         angle_tol=angle_tol*2;
+%         angle_tol=angle_tol+angle_range/2;
 %     elseif len<n
-%         angle_tol = angle_tol/2;
+%         angle_tol = angle_tol-angle_range/2;
 %     end
+%     angle_range = angle_range/2;
 % end
 imshow(imdilate(blank,se));
 end
