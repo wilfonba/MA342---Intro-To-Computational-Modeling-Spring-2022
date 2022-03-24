dc9b936b10d2966a354b13b21e6701d23b65299f
guetigtb <101212578+guetigtb@users.noreply.github.com>
2022-03-24 14:36:15 -0400

    Version: 03/24/2022

--- Project02/ma342_proj2_wave_eqn.m
+++ Project02/ma342_proj2_wave_eqn.m
@@ -7,7 +7,7 @@ clc; close all; clear variables;
 %% Problem 2 (Adding a damping term to the example)
 
 %Problem setup
-kappa = 0.35;                                %Selected values of kappa to sees the effects of a damper
+kappa = 0.9758;                                %Selected values of kappa to sees the effects of a damper
 
 L = 2;                                      %Overall length of the wire
 T = 30;                                     %Maximum time
@@ -26,9 +26,11 @@ Sol_damped(:,2) = (exp(-10*(PosX-(L/2)).^2) ...
     - exp(-10*(L/2).^2))*dt;                    %Applies the initial condition for du/dt(x,t=0)
 
 %For Loop values
+Tdip = zeros(30,1);
 r = alpha*dt^2 / dx^2;                      %Represented constant H/rho * (delta t)^2/(delta x)^2
 indx = 2:length(PosX)-1;                    %Covers all columns (spatial components) of the solution matrix in the for loop from 2 to 200
 image_count = 0;                            %Sets a counter variables for for loop for animation
+counter = 1;
 
 %Solve the problem
 for j = 3:length(Time)                      %Covers all the rows (time components) of the solution matrix with a for loop
@@ -47,7 +49,17 @@ for j = 3:length(Time)                      %Covers all the rows (time component
         kappa*dt*Sol_damped(indx,j-1)/(kappa*dt+1) + ...
         (2*Sol_damped(indx,j-1) - Sol_damped(indx,j-2))/(kappa*dt+1);     %Fills the solution matrix for all time and positions
     
-    if image_count >= 10000                     %Once counter reaches limit, update animation
+    if Sol_damped(indx,j) < 0 
+        if counter == 1
+            Tdip(counter) = Time(j);
+            fprintf('Our string dips below zero at %f sec \n',Tdip(counter));
+            counter = counter + 1;
+        end
+    end
+
+
+    
+    if image_count >= 10                     %Once counter reaches limit, update animation
         figure(1)
         subplot(2,1,2)
         plot(Sol_damped(indx,j),'r-')
@@ -71,18 +83,18 @@ end
 
 %% Problem 3 (Wave equation on a unit disk)
 
-circle_size = 1;                        %x^2 + y^2 result
+r = 1;                        %x^2 + y^2 result or r^2
 Tmax = 20;                              %Maximum time
-dt = 0.05;
-dx = 0.01;
-dy = 0.01;
-PosX = unique([0:dx:sqrt(circle_size),sqrt(circle_size)]);
-PosY = unique([0:dx:sqrt(circle_size),sqrt(circle_size)]);
+dt = 0.2;
+dx = 0.05;
+dy = 0.05;
+PosX = unique([-sqrt(r):dx:sqrt(r),sqrt(r)]);
+PosY = unique([-sqrt(r):dy:sqrt(r),sqrt(r)]);
 Time = unique([0:dt:Tmax,Tmax]);
 Sol_disk = zeros(length(PosX), length(PosY), length(Time));
 
 %Apply BC
-Sol_disk(:,:,1) = sqrt(circle_size)/10;
+Sol_disk(:,:,1) = sqrt(r)/10;
 
 %For Loop values
 indxX = 2:length(PosX)-1;
