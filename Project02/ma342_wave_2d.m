8192ce6a0bfcd2f9c511964287f3d3b9ed6507c0
wilfonba <48168887+wilfonba@users.noreply.github.com>
2022-03-28 14:37:11 -0400

    finalized wave equation

--- Project02/ma342_wave_2d.m
+++ Project02/ma342_wave_2d.m
@@ -2,24 +2,36 @@
 
 clc; close all; clear variables;
 
-Nxy = 24;
-Tmax = 1; %Maximum time
+
+norm = 2;
+Nxy = 128;
+Tmax = 5; %Maximum time
 dx = 1/(Nxy-1);
-dt = 1/12*dx^2;
+dt = 0.95*1/4*dx^2;
 Time = unique([0:dt:Tmax,Tmax]);
 frameSteps = floor(0.005/dt);
 
 
-[PosX, PosY, NTA] = waveEquationMesh(Nxy, 3, 0);
+[PosX, PosY, NTA] = waveEquationMesh(Nxy, norm, 0);
 dx = max(PosX(2:end) - PosX(1:end-1));
 dy = max(PosY(2:end) - PosY(1:end-1));
-Sol_disk = zeros(length(PosX), length(PosY), length(Time));
+Sol_disk = zeros(length(PosX), length(PosY), 3);
 [X,Y] = meshgrid(PosX,PosY);
 Y = flipud(Y);
 
 %Apply BC
-Sol_disk(:,:,1) = (PosX.^0 + flip(PosY).^2)/10 .* ones(Nxy);
-Sol_disk(:,:,2) = zeros(Nxy);
+Sol_disk(:,:,1) = (X.^norm + Y.^norm)/10;
+for i = 1:Nxy
+    for j = 1:Nxy
+        if NTA(i,j) == 0
+            Sol_disk(i,j) = NaN;
+        elseif NTA(i,j) == 2
+            Sol_disk(i,j) = 0.1;
+        end
+    end
+end
+Sol_disk(:,:,2) = Sol_disk(:,:,1) + dt.*zeros(Nxy);
+Sol_disk(:,:,3) = NaN;
 
 f = waitbar(0,'solution progress');
 fig = figure();
@@ -28,50 +40,57 @@ fig.Visible = "off";
 writerObj = VideoWriter('WaveEquationVideosAndData/WaveEquation','MPEG-4');
 open(writerObj);
 
+x = 1;y = 2;z = 3;
+idxs = [1 2 3];
 
 image_count = 0;
 for j = 3:length(Time)
     for indxX = 1:Nxy
         for indxY = 1:Nxy
             if NTA(indxX,indxY) == 3
-                Sol_disk(indxX,indxY,j) = 2*dt^2/dx^2 * (Sol_disk(indxX+1,indxY,j-1) - ...
-                    2*Sol_disk(indxX,indxY,j-1) + Sol_disk(indxX-1,indxY,j-1)) + ...
-                    2*dt^2/dy^2 * (2*Sol_disk(indxX,indxY+1,j-1) - 2*Sol_disk(indxX,indxY,j-1)) + ...
-                    2*Sol_disk(indxX,indxY,j-1) - Sol_disk(indxX,indxY,j-2);                              %Corrected for symmetry only in x dir
+                Sol_disk(indxX,indxY,z) = 2*dt^2/dx^2 * (Sol_disk(indxX+1,indxY,y) - ...
+                    2*Sol_disk(indxX,indxY,y) + Sol_disk(indxX-1,indxY,y)) + ...
+                    2*dt^2/dy^2 * (2*Sol_disk(indxX,indxY+1,y) - 2*Sol_disk(indxX,indxY,y)) + ...
+                    2*Sol_disk(indxX,indxY,y) - Sol_disk(indxX,indxY,x);                              %Corrected for symmetry only in x dir
             elseif NTA(indxX,indxY) == 4
-                Sol_disk(indxX,indxY,j) = 2*dt^2/dx^2 * (Sol_disk(indxX-1,indxY,j-1) - ...
-                    2*Sol_disk(indxX,indxY,j-1) + Sol_disk(indxX-1,indxY,j-1)) + ...
-                    2*dt^2/dy^2 * (Sol_disk(indxX,indxY+1,j-1) - 2*Sol_disk(indxX,indxY,j-1) + ...
-                    Sol_disk(indxX,indxY-1,j-1)) + 2*Sol_disk(indxX,indxY,j-1) - ...
-                    Sol_disk(indxX,indxY,j-2);                              %Corrected for symmetry only in y dir
+                Sol_disk(indxX,indxY,z) = 2*dt^2/dx^2 * (Sol_disk(indxX-1,indxY,y) - ...
+                    2*Sol_disk(indxX,indxY,y) + Sol_disk(indxX-1,indxY,y)) + ...
+                    2*dt^2/dy^2 * (Sol_disk(indxX,indxY+1,y) - 2*Sol_disk(indxX,indxY,y) + ...
+                    Sol_disk(indxX,indxY-1,y)) + 2*Sol_disk(indxX,indxY,y) - ...
+                    Sol_disk(indxX,indxY,x);                              %Corrected for symmetry only in y dir
             elseif NTA(indxX,indxY) == 5
-                Sol_disk(indxX,indxY,j) = 2*dt^2/dx^2 * (Sol_disk(indxX-1,indxY,j-1) - ...
-                    2*Sol_disk(indxX,indxY,j-1) + Sol_disk(indxX-1,indxY,j-1)) + ...
-                    2*dt^2/dy^2 * (Sol_disk(indxX,indxY+1,j-1) - 2*Sol_disk(indxX,indxY,j-1) + ...
-                    Sol_disk(indxX,indxY+1,j-1)) + 2*Sol_disk(indxX,indxY,j-1) - ...
-                    Sol_disk(indxX,indxY,j-2);                              %Corrected for symmetry in x and y dir
+                Sol_disk(indxX,indxY,z) = 2*dt^2/dx^2 * (Sol_disk(indxX-1,indxY,y) - ...
+                    2*Sol_disk(indxX,indxY,y) + Sol_disk(indxX-1,indxY,y)) + ...
+                    2*dt^2/dy^2 * (Sol_disk(indxX,indxY+1,y) - 2*Sol_disk(indxX,indxY,y) + ...
+                    Sol_disk(indxX,indxY+1,y)) + 2*Sol_disk(indxX,indxY,y) - ...
+                    Sol_disk(indxX,indxY,x);                              %Corrected for symmetry in x and y dir
             elseif NTA(indxX,indxY) == 1
-                Sol_disk(indxX,indxY,j) = 2*dt^2/dx^2 * (Sol_disk(indxX+1,indxY,j-1) - ...
-                    2*Sol_disk(indxX,indxY,j-1) + Sol_disk(indxX-1,indxY,j-1)) + ...
-                    2*dt^2/dy^2 * (Sol_disk(indxX,indxY+1,j-1) - 2*Sol_disk(indxX,indxY,j-1) + ...
-                    Sol_disk(indxX,indxY-1,j-1)) + 2*Sol_disk(indxX,indxY,j-1) - ...
-                    Sol_disk(indxX,indxY,j-2);
+                Sol_disk(indxX,indxY,z) = 2*dt^2/dx^2 * (Sol_disk(indxX+1,indxY,y) - ...
+                    2*Sol_disk(indxX,indxY,y) + Sol_disk(indxX-1,indxY,y)) + ...
+                    2*dt^2/dy^2 * (Sol_disk(indxX,indxY+1,y) - 2*Sol_disk(indxX,indxY,y) + ...
+                    Sol_disk(indxX,indxY-1,y)) + 2*Sol_disk(indxX,indxY,y) - ...
+                    Sol_disk(indxX,indxY,x);
             elseif NTA(indxX,indxY) == 0
-                Sol_disk(indxX,indxY,j) = NaN;
+                Sol_disk(indxX,indxY,z) = NaN;
             else
-                Sol_disk(indxX,indxY,j) = max((PosX.^2 + flip(PosY).^2)/10);
+                Sol_disk(indxX,indxY,z) = 0.1;
             end
         end
     end
     
-    if image_count > 2
+    idxs = circshift(idxs,-1);
+    x = idxs(1);
+    y = idxs(2);
+    z = idxs(3);
+    
+    if image_count > frameSteps
         cla;
-        surf(X,Y,Sol_disk(:,:,j),'EdgeColor','none');hold on;
-        surf(-X,Y,Sol_disk(:,:,j),'EdgeColor','none')
-        surf(-X,-Y,Sol_disk(:,:,j),'EdgeColor','none')
-        surf(X,-Y,Sol_disk(:,:,j),'EdgeColor','none')
+        surf(X,Y,Sol_disk(:,:,z),'EdgeColor','none');hold on;
+        surf(-X,Y,Sol_disk(:,:,z),'EdgeColor','none')
+        surf(-X,-Y,Sol_disk(:,:,z),'EdgeColor','none')
+        surf(X,-Y,Sol_disk(:,:,z),'EdgeColor','none')
         colormap("default")
-        axis([-1, 1, -1, 1, -2, 2])
+        axis([-1, 1, -1, 1, -0.05, 0.25])
         hold off;
         image_count = 0;
         writeVideo(writerObj,getframe(gcf)); 
