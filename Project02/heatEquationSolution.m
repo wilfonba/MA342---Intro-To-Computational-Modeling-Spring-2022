300a0d956b95551c27576e9d9aca09626efcb92e
Bruner <alanjbruner@gmail.com>
2022-04-03 02:21:02 -0400

    Added Mex File (finally)

--- Project02/heatEquationSolution.m
+++ Project02/heatEquationSolution.m
@@ -57,8 +57,10 @@ Y = flipud(Y);
 fig = figure("position",[50 50 1000 800]);
 fig.Visible = 'off';
 
-N = ceil(2/dt);
+ N = ceil(2/dt);
+% N = 10000;
 stepsPerFrame = floor(frameDt/dt);
+% stepsPerFrame = 100;
 %N = 10;
 
 fprintf("Dt = %1.5e, N = %1.5e \n",dt,N);
@@ -83,39 +85,40 @@ wBar = waitbar(0,"Solution Progress");
 
 for i = 1:N
     tic;
-    for j = 1:Ny
-        for k = 1:Nx
-            if NTA(j,k) == 3
-                T(j,k,m) = T(j,k,n) + const*(T(j+1,k,n) - 2*T(j,k,n) + T(j-1,k,n)) + ...
-                    const*(T(j,k+1,n) - 2*T(j,k,n) + T(j,k-1,n));
-            elseif NTA(j,k) == 4
-                T(j,k,m) = T(j,k,n) + const*(T(j+1,k,n) - 2*T(j,k,n) + T(j-1,k,n)) + ...
-                    const*(2*T(j,k+1,n) - 2*T(j,k,n));
-            elseif NTA(j,k) == 5
-                T(j,k,m) = T(j,k,n) + const*(2*T(j-1,k,n) - 2*T(j,k,n)) + ...
-                    const*(T(j,k+1,n) - 2*T(j,k,n) + T(j,k-1,n));
-            elseif NTA(j,k) == 1
-                R = [xs(k)-0.05,ys(end-j+1)];
-                R = R./norm(R,2);
-                T(j,k,m) = ((3*R(1)+3*R(2))/(2*h))^(-1)*...
-                    (-1 + R(1)/(2*h)*(4*T(j,k-1,n) - T(j,k-2,n)) + R(2)/(2*h)*(-T(j+2,k,n)+4*T(j+1,k,n)));
-            elseif NTA(j,k) == 2
-                T(j,k,m) = 60;
-            elseif NTA(j,k) == 0
-                T(j,k,m) = NaN;
-            elseif NTA(j,k) == 7
-                R = [xs(k)-0.05,ys(end-j+1)];
-                R = R./norm(R,2);
-                T(j,k,m) = ((3*R(1)+4*R(2))/(2*h))^(-1)*(-1 + R(1)/(2*h)*...
-                    (4*T(j,k-1,n) - T(j,k-2,n)) + 2*R(2)/(h)*T(j+1,k,n));
-            elseif NTA(j,k) == 8
-                T(j,k,m) = -2*h/3 + (4/3)*T(j,k-1,n) - (1/3)*T(j,k-2,n);
-            else              %6
-                T(j,k,m) = T(j,k,n) + const*(2*T(j-1,k,n) - 2*T(j,k,n)) + ...
-                    const*(2*T(j,k+1,n) - 2*T(j,k,n));
-            end
-        end
-    end
+    T = updateT(NTA,T,Ny,Nx,const,h,n,m,length(ys),xs,ys);
+%     for j = 1:Ny
+%         for k = 1:Nx
+%             if NTA(j,k) == 3
+%                 T(j,k,m) = T(j,k,n) + const*(T(j+1,k,n) - 2*T(j,k,n) + T(j-1,k,n)) + ...
+%                     const*(T(j,k+1,n) - 2*T(j,k,n) + T(j,k-1,n));
+%             elseif NTA(j,k) == 4
+%                 T(j,k,m) = T(j,k,n) + const*(T(j+1,k,n) - 2*T(j,k,n) + T(j-1,k,n)) + ...
+%                     const*(2*T(j,k+1,n) - 2*T(j,k,n));
+%             elseif NTA(j,k) == 5
+%                 T(j,k,m) = T(j,k,n) + const*(2*T(j-1,k,n) - 2*T(j,k,n)) + ...
+%                     const*(T(j,k+1,n) - 2*T(j,k,n) + T(j,k-1,n));
+%             elseif NTA(j,k) == 1
+%                 R = [xs(k)-0.05,ys(end-j+1)];
+%                 R = R./norm(R,2);
+%                 T(j,k,m) = ((3*R(1)+3*R(2))/(2*h))^(-1)*...
+%                     (-1 + R(1)/(2*h)*(4*T(j,k-1,n) - T(j,k-2,n)) + R(2)/(2*h)*(-T(j+2,k,n)+4*T(j+1,k,n)));
+%             elseif NTA(j,k) == 2
+%                 T(j,k,m) = 60;
+%             elseif NTA(j,k) == 0
+%                 T(j,k,m) = NaN;
+%             elseif NTA(j,k) == 7
+%                 R = [xs(k)-0.05,ys(end-j+1)];
+%                 R = R./norm(R,2);
+%                 T(j,k,m) = ((3*R(1)+4*R(2))/(2*h))^(-1)*(-1 + R(1)/(2*h)*...
+%                     (4*T(j,k-1,n) - T(j,k-2,n)) + 2*R(2)/(h)*T(j+1,k,n));
+%             elseif NTA(j,k) == 8
+%                 T(j,k,m) = -2*h/3 + (4/3)*T(j,k-1,n) - (1/3)*T(j,k-2,n);
+%             else              %6
+%                 T(j,k,m) = T(j,k,n) + const*(2*T(j-1,k,n) - 2*T(j,k,n)) + ...
+%                     const*(2*T(j,k+1,n) - 2*T(j,k,n));
+%             end
+%         end
+%     end
 
     if (mod(i,stepsPerFrame) == 0)
         fprintf("Frame: %i\n",i/stepsPerFrame);
---
+++ Project02/updateT.c
@@ -0,0 +1,149 @@
+#include <mex.h>
+#include <math.h>
+
+double nY,nX,con,h;
+int n,m,yLength;
+double *xs,*ys;
+
+int getIndex(int i, int j, int k) {
+    return i+j*(int)(nY+0.05)+(k-1)*(int)(nY+0.05)*(int)(nX+0.05);
+}
+
+// NTA T nY, nX, con, h, n, m, yLength
+void mexFunction(int numInputs , mxArray *output[],int
+numOutputs , const mxArray *input[])   // define the main function  
+{  
+    // Define Variables
+    double* NTA;
+    double* T;
+    int i;
+    mxArray *fOut;
+    double* outVals;
+    mwSize dimNum;
+    mwSize dims[3];
+    int j,k;
+    double r1,r2,normR;
+
+    // Associate Inputs
+    NTA = mxGetPr(input[0]);
+    output[0] = mxDuplicateArray(input[1]);
+    fOut = output[0];
+    T = mxGetPr(fOut);
+    xs = mxGetPr(input[9]);
+    ys = mxGetPr(input[10]);
+    nY = *mxGetPr(input[2]);
+    nX = *mxGetPr(input[3]);
+    con = *mxGetPr(input[4]);
+    h = *mxGetPr(input[5]);
+    n = *mxGetPr(input[6]);
+    m = *mxGetPr(input[7]);
+    yLength = *mxGetPr(input[8]);
+    // Associate Outputs
+    for(j=0;j<nY;j++) {
+        for(k=0;k<nX;k++) {
+            // printf("index: %d\n",getIndex(j,k,1));
+            // printf("NTA[%d %d] = %f\n",j,k,NTA[getIndex(j,k,0)]);
+            T[getIndex(j,k,m)] = -1;
+            switch((int)(NTA[getIndex(j,k,1)]+0.05)) {
+                case 0 :
+                    T[getIndex(j,k,m)] = NAN;
+                    break;
+                case 1 :
+//                 R = [xs(k)-0.05,ys(end-j+1)];
+//                 R = R./norm(R,2);
+//                 T(j,k,m) = ((3*r1+3*r2))/(2*h))^(-1)*(-1 + r1/(2*h)*(4*T[getIndex(j,k-1,n)] - T[getIndex(j,k-2,n)]) + r2/(2*h)*(-T[getIndex(j+2,k,n)]+4*T[getIndex(j+1,k,n)]));
+                    r1 = (xs[k]);
+                    r2 = ys[yLength-j+1];
+                    normR = sqrt(r1*r1+r2*r2);
+                    r1 = r1/normR;
+                    r2 = r2/normR;
+                    
+                    T[getIndex(j,k,m)] = (-1 + r1/(2*h)*(4*T[getIndex(j,k-1,n)] - T[getIndex(j,k-2,n)]) + r2/(2*h)*(-T[getIndex(j+2,k,n)]+4*T[getIndex(j+1,k,n)]))/(((3*r1+3*r2))/(2*h));
+                    // printf("norm=%f\n",normR);
+                    // printf("r1=%f\n",r1);
+                    // printf("r2=%f\n",r2);
+                    // printf("k-1: %f\n",T[getIndex(j,k-1,n)]);
+                    // printf("k-2: %f\n",T[getIndex(j,k-2,n)]);
+                    // printf("j+1: %f\n",T[getIndex(j+1,k,n)]);
+                    // printf("j+2: %f\n",T[getIndex(j,k-1,n)]);
+                
+                    break;
+
+                case 2 :
+                    T[getIndex(j,k,m)] = 60;
+                    break;
+
+                case 3 :
+                    T[getIndex(j,k,m)] = T[getIndex(j,k,n)] + con*(T[getIndex(j+1,k,n)] - 2*T[getIndex(j,k,n)] + T[getIndex(j-1,k,n)]) + 
+                        con*(T[getIndex(j,k+1,n)] - 2*T[getIndex(j,k,n)] + T[getIndex(j,k-1,n)]);
+                    break;
+
+                case 4 :
+                T[getIndex(j,k,m)] = T[getIndex(j,k,n)] + con*(T[getIndex(j+1,k,n)] - 2*T[getIndex(j,k,n)] + T[getIndex(j-1,k,n)]) + 
+                     con*(2*T[getIndex(j,k+1,n)] - 2*T[getIndex(j,k,n)]);
+                     if(isnan(T[getIndex(j,k,m)])) {
+                        printf("Case: %f\n",NTA[getIndex(j,k,1)]);
+                    }
+                    break;
+
+                case 5 :
+                    T[getIndex(j,k,m)] = T[getIndex(j,k,n)]+con*(
+                        2*T[getIndex(j-1,k,n)]-2*
+                        T[getIndex(j,k,n)])+con*(
+                            (T[getIndex(j,k+1,n)]-2*
+                            T[getIndex(j,k,n)]+
+                            T[getIndex(j,k-1,n)]));
+                            if(isnan(T[getIndex(j,k,m)])) {
+                        printf("Case: %f\n",NTA[getIndex(j,k,1)]);
+                    }
+                    break;
+
+                case 6 :
+                    T[getIndex(j,k,m)] = T[getIndex(j,k,n)]+con*(
+                        2*(T[getIndex(j-1,k,n)])-
+                        2*(T[getIndex(j,k,n)]))+con*(
+                        2*(T[getIndex(j,k+1,n)])-
+                        2*T[getIndex(j,k,n)]);
+                        if(isnan(T[getIndex(j,k,m)])) {
+                        printf("Case: %f\n",NTA[getIndex(j,k,1)]);
+                    }
+                    break;
+
+                case 7 : 
+                 r1 = xs[k]-0.05;
+                    r2 = ys[yLength-j+1];
+                    normR = sqrt(r1*r1+r2*r2);
+                    r1 = r1/normR;
+                    r2 = r2/normR;
+                    T[getIndex(j,k,m)] = (-1+(r1/(2*h))*(4*
+                        (T[(getIndex(j,k-1,n))]) -
+                        (T[j,k-2,n]))+2*
+                        r2/h*T[getIndex(j+1,k,n)])/
+                        ((3*r1+4*r2)/(2*h));
+                    if(isnan(T[getIndex(j,k,m)])) {
+                        printf("Case: %f\n",NTA[getIndex(j,k,1)]);
+                    }
+                    break;
+
+                case 8 :
+                //T(j,k,m) = -2*h/3 + (4/3)*T(j,k-1,n) - (1/3)*T(j,k-2,n);
+                    T[getIndex(j,k,m)] = -2*h/3+(4/3)*T[getIndex(j,k-1,n)]-(1/3)*T[getIndex(j,k-2,n)];
+                    if(isnan(T[getIndex(j,k,m)])) {
+                        printf("Case: %f\n",NTA[getIndex(j,k,1)]);
+                    }
+                    break;
+
+                default :
+                    T[getIndex(j,k,m)] = -1;
+                    break;
+            if(isnan(T[getIndex(j,k,m)])) {
+                T[getIndex(j,k,m)] = T[getIndex(j,k,n)];
+            }
+            }
+        }
+    }
+    // printf("NTA[0]=%f\n",NTA[0]);
+    // output[0] = fOut;
+
+
+}
