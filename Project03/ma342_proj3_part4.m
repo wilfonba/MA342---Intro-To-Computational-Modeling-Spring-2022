f4847378f3a3305d01fbae9c147af47f92aa8cee
guetigtb <101212578+guetigtb@users.noreply.github.com>
2022-04-24 18:52:41 -0400

    Most Recent Version

--- Project03/ma342_proj3_part4.m
+++ Project03/ma342_proj3_part4.m
@@ -4,23 +4,25 @@
 
 clc; close all; clear variables;
 
-tau = 5;
+tau = 0.5;
 N = 100;
 end_time = 10;
 tspan = [0, end_time];
 
 sol = ddensd(@ddefun,@(t,y)dely(t,y,tau),@(t,y)delyp(t,y,tau),@history,tspan);
 
-x_hist = linspace(-tau/2,0,N);
+x_hist = linspace(-end_time/4,0,N);
 y_hist = history(x_hist);
 
 figure(1)
 hold on
 plot(x_hist,y_hist(1,:),'b--',x_hist,y_hist(2,:),'r--',x_hist,y_hist(3,:),'k--')
-plot(sol.x,sol.y(1,:),'b-',sol.x,sol.y(2,:),'r-',sol.x,sol.y(3,:),'k-')
+p2 = plot(sol.x,sol.y(1,:),'b-',sol.x,sol.y(2,:),'r-',sol.x,sol.y(3,:),'k-');
 xlabel('Time(t)')
 ylabel('y(t)')
-axis([-tau/2 end_time -1.2 1.2])
+%title(sprintf(' \tau = %f',tau))
+%legend([p2(1), p2(2), p2(3)],'h(t)=sin(t)','h(t)=sin(2t)','h(t)=sin(3t)')
+axis([-end_time/4 end_time -1.2 1.2])
 
 function [yp] = ddefun(t,y,ydel,dydel)
 L = 100*[-7 1 2; 3 -9 0; 1 2 -6];
