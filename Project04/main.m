9313c6de9afdc8d9039a9a6d3ca364418efba871
wilfonba <48168887+wilfonba@users.noreply.github.com>
2022-04-28 14:10:56 -0400

    more fixing

--- Project04/main.m
+++ Project04/main.m
@@ -1,8 +1,8 @@
 clc;clear;close all;
+%% Read Data
+alpha = 0.5;
+[sigma,r,C,stonks] = readStonksHistory(3,[],'1d','Mean',"stonks.csv");
 
-alpha = 0.1;
-[sigma,r,C,stonks] = readStonksHistory("1-Jul-2014","31-Dec-2014",'1d','Mean',"exampleStonks.csv");
-
-
+%% Optimize
 N = size(sigma,1);
-[w,optVal] = quadprog((1-alpha)*2*C, -alpha*r,[],[],ones(1,N),[1],zeros(N,1),0.2*ones(N,1));
+[w,optVal] = quadprog((1-alpha)*2*C, -alpha*r,[],[],ones(1,N),[1],0.01*ones(N,1),0.2*ones(N,1));
