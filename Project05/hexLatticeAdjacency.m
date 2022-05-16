ebfd8aabf3e6889a27cbb87050c4ee34a7c2d399
guetigtb <101212578+guetigtb@users.noreply.github.com>
2022-05-16 14:43:31 -0400

    Tested with N = 6

--- Project05/hexLatticeAdjacency.m
+++ Project05/hexLatticeAdjacency.m
@@ -1,8 +1,28 @@
-1function A = hexLatticeAdjacency(N)
+function [A] = hexLatticeAdjacency(N)
+% Records the adjacency top, bottom, and left or right depending on the
+% node's position
 
-    nodes = 1:N*(N-1);
-    nodes = reshape(nodes,N,N-1)
-    
-    
+a = [(1:N^2-N)',(N+1:N^2)'];
+e = [1:1:N,N^2-N+1:1:N^2]';
+e = reshape(e,N,2);
+b = [];
+for i = 1:N
+   c = zeros(N-1,2);
+   for j = 1:N-1
+       c(j,:) = [(i-1)*N+j,(i-1)*N + j + 1];
+   end
+   b = [b;c];
+end
+d = [1:N:N^2,N:N:N^2];
+d = reshape(d,N,2);
+for index = length(b):-2:2
+    b(index,:) = [];
+end
+for index = length(d)-1:-2:1
+    d(index,:) = [];
+end
+h = [b;d];
+
+A = [a;e;h];
 
 end
